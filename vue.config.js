eb0791c33e1f14a1a4d42560af6ae0367c5b433c
Loc Pham <phamducloc2003@gmail.com>
2024-08-09 00:14:16 +0700

    Undo path reconfiguration

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,3 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
-
-module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
-};
